717d36e6c5f429548ee6b029bd33bedc957957b2
Ted Kassen <tedgkassen@gmail.com>
2019-10-20 13:49:42 -0700

    ok frontend finally works

--- assets/src/app/main.cljs
+++ assets/src/app/main.cljs
@@ -1,4 +1,12 @@
-(ns app.main)
+(ns app.main
+  (:require [reagent.core :as r :refer [atom]]))
 
-(defn main! []
-  (println "Hello World!"))
+(defn simple-component []
+  [:div
+   [:p "I am a component!"]])
+
+(defn ^:dev/after-load main! []
+  (println "Hello World!")
+  (r/render-component
+   [simple-component]
+   (.getElementById js/document "app")))
